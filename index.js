72b1b6d6cba19330d13f8d805d29e75bbed4626a
Bennett <69775498+supercat3point0@users.noreply.github.com>
2025-10-27 15:58:29 -0700

    Clean up

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateBaseAddress(a) {
         a = 0xfff00;
     a -= a % 16;
     emulator.ui.elements.namedItem("base-address").value = a.toString(16).toUpperCase().padStart(5, "0");
-    const rows = document.getElementById("memory-view")?.querySelectorAll("tbody tr");
+    const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
     for (let i = 0; i < 16; i++) {
         const row = rows.item(i);
         row.querySelector("th").innerHTML = (a + i * 0x10).toString(16).toUpperCase().padStart(5, "0");
@@ -15,12 +15,12 @@ function updateBaseAddress(a) {
     emulator.baseAddress = a;
 }
 function updateMemoryView() {
-    const rows = document.getElementById("memory-view")?.querySelectorAll("tbody tr");
+    const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
     for (let i = 0; i < 16; i++) {
         const cells = rows.item(i).querySelectorAll("td input");
         for (let j = 0; j < 16; j++) {
             const cell = cells.item(j);
-            cell.value = w86.HEAPU8[emulator.state.memory + emulator.baseAddress + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
+            cell.value = emulator.memory[emulator.baseAddress + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
         }
     }
 }
@@ -42,6 +42,20 @@ function updateDisplay() {
         emulator.ui.elements.namedItem("flags" + i.toString()).checked = (emulator.state.registers.flags >> i & 1) !== 0;
     updateMemoryView();
 }
+function stepEmulator() {
+    switch (w86.w86CpuStep(emulator.state)) {
+        case w86.W86Status.SUCCESS:
+            break;
+        case w86.W86Status.UNDEFINED_OPCODE:
+            console.error(`Undefined opcode: 0x${emulator.memory[((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)].toString(16).toUpperCase().padStart(2, "0")}`);
+            break;
+        case w86.W86Status.UNIMPLEMENTED_OPCODE:
+            console.error(`Unimplemented opcode: 0x${emulator.memory[((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)].toString(16).toUpperCase().padStart(2, "0")}`);
+            break;
+        default:
+            console.error("Unknown error");
+    }
+}
 function resetEmulator() {
     emulator.state.registers = {
         ax: 0x0000,
@@ -59,10 +73,10 @@ function resetEmulator() {
         ip: 0x0000,
         flags: 0x0000
     };
-    w86.HEAPU8.subarray(emulator.state.memory, emulator.state.memory + emulator.memorySize).set(new Uint8Array(emulator.rom));
+    emulator.memory.set(emulator.rom);
 }
 {
-    const rows = document.getElementById("memory-view")?.querySelectorAll("tbody tr");
+    const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
     const byte = document.createElement("input");
     byte.type = "text";
     byte.disabled = true;
@@ -85,7 +99,9 @@ const w86 = await W86();
 const emulator = {
     state: new w86.W86CpuState(),
     memorySize: 1048576,
-    rom: new ArrayBuffer(1048576),
+    memory: new Uint8Array(),
+    _rom: new ArrayBuffer(1048576),
+    rom: new Uint8Array(),
     baseAddress: 0x00000,
     ui: document.getElementById("emulator")
 };
@@ -106,9 +122,10 @@ emulator.state.registers = {
     flags: 0x0000
 };
 emulator.state.memory = w86._malloc(emulator.memorySize);
-w86.HEAPU8.subarray(emulator.state.memory, emulator.state.memory + emulator.memorySize).fill(0);
+emulator.memory = w86.HEAPU8.subarray(emulator.state.memory, emulator.state.memory + emulator.memorySize).fill(0);
+emulator.rom = new Uint8Array(new ArrayBuffer(emulator.memorySize));
 emulator.ui.elements.namedItem("step").addEventListener("click", () => {
-    w86.w86CpuStep(emulator.state);
+    stepEmulator();
     updateDisplay();
 });
 emulator.ui.elements.namedItem("reset").addEventListener("click", () => {
@@ -117,8 +134,7 @@ emulator.ui.elements.namedItem("reset").addEventListener("click", () => {
 });
 emulator.ui.elements.namedItem("rom").addEventListener("change", (event) => {
     event.currentTarget.files?.item(0)?.arrayBuffer().then((buf) => {
-        const view = new Uint8Array(emulator.rom);
-        view.fill(0).set(new Uint8Array(buf).subarray(0, emulator.memorySize));
+        emulator.rom.fill(0).set(new Uint8Array(buf).subarray(0, emulator.memorySize));
         resetEmulator();
         updateDisplay();
     });

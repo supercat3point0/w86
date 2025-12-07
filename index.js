300835be43936ab9c56f7b8f3fcfbe877d1ae9fb
Bennett <69775498+supercat3point0@users.noreply.github.com>
2025-12-07 14:28:03 -0800

    UI additions

--- index.js
+++ index.js
@@ -1,45 +1,86 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 import W86, {} from "./w86.js";
-function updateBaseAddress(a) {
+function updateMemoryBase(a) {
     if (a < 0x00000)
         a = 0x00000;
     if (a > 0xfff00)
         a = 0xfff00;
     a -= a % 16;
-    emulator.ui.elements.namedItem("base-address").value = a.toString(16).toUpperCase().padStart(5, "0");
+    emulator.ui.elements.namedItem("memory-base").value = a.toString(16).toUpperCase().padStart(5, "0");
     const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
     for (let i = 0; i < 16; i++) {
         const row = rows.item(i);
         row.querySelector("th").innerHTML = (a + i * 0x10).toString(16).toUpperCase().padStart(5, "0");
     }
-    emulator.baseAddress = a;
+    emulator.base.memory = a;
 }
-function updateMemoryView() {
-    const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
+function updateProgramBase(a) {
+    if (a < 0x00000)
+        a = 0x00000;
+    if (a > 0xfff00)
+        a = 0xfff00;
+    a -= a % 16;
+    emulator.ui.elements.namedItem("program-base").value = a.toString(16).toUpperCase().padStart(5, "0");
+    const rows = document.getElementById("program-view").querySelectorAll("tbody tr");
     for (let i = 0; i < 16; i++) {
-        const cells = rows.item(i).querySelectorAll("td input");
-        for (let j = 0; j < 16; j++) {
-            const cell = cells.item(j);
-            cell.value = emulator.memory[emulator.baseAddress + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
-        }
+        const row = rows.item(i);
+        row.querySelector("th").innerHTML = (a + i * 0x10).toString(16).toUpperCase().padStart(5, "0");
     }
+    emulator.base.program = a;
+}
+function updateIoReadsBase(a) {
+    if (a < 0x0000)
+        a = 0x0000;
+    if (a > 0xff00)
+        a = 0xff00;
+    a -= a % 16;
+    emulator.ui.elements.namedItem("io-reads-base").value = a.toString(16).toUpperCase().padStart(4, "0");
+    const rows = document.getElementById("io-reads-view").querySelectorAll("tbody tr");
+    for (let i = 0; i < 16; i++) {
+        const row = rows.item(i);
+        row.querySelector("th").innerHTML = (a + i * 0x10).toString(16).toUpperCase().padStart(4, "0");
+    }
+    emulator.base.io.reads = a;
+}
+function updateIoWritesBase(a) {
+    if (a < 0x0000)
+        a = 0x0000;
+    if (a > 0xff00)
+        a = 0xff00;
+    a -= a % 16;
+    emulator.ui.elements.namedItem("io-writes-base").value = a.toString(16).toUpperCase().padStart(4, "0");
+    const rows = document.getElementById("io-writes-view").querySelectorAll("tbody tr");
+    for (let i = 0; i < 16; i++) {
+        const row = rows.item(i);
+        row.querySelector("th").innerHTML = (a + i * 0x10).toString(16).toUpperCase().padStart(4, "0");
+    }
+    emulator.base.io.writes = a;
 }
 function updateDisplay() {
     const execState = emulator.ui.elements.namedItem("exec-state");
-    if (emulator.execState.run) {
-        emulator.ui.elements.namedItem("run").classList.add("hidden");
-        emulator.ui.elements.namedItem("stop").classList.remove("hidden");
-        execState.classList.replace("status-stop", "status-run");
+    if (!emulator.execState.error) {
+        if (emulator.execState.run) {
+            emulator.ui.elements.namedItem("run").classList.add("hidden");
+            emulator.ui.elements.namedItem("stop").classList.remove("hidden");
+            execState.classList.replace("status-stop", "status-run");
+        }
+        else {
+            emulator.ui.elements.namedItem("run").classList.remove("hidden");
+            emulator.ui.elements.namedItem("stop").classList.add("hidden");
+            execState.classList.replace("status-run", "status-stop");
+            execState.classList.replace("status-halt", "status-stop");
+        }
+        if (emulator.execState.halt)
+            execState.classList.replace("status-run", "status-halt");
+        execState.value = `${emulator.execState.run ? "Running" : "Stopped"}${emulator.execState.halt ? " (Halted)" : ""}`;
     }
     else {
         emulator.ui.elements.namedItem("run").classList.remove("hidden");
         emulator.ui.elements.namedItem("stop").classList.add("hidden");
         execState.classList.replace("status-run", "status-stop");
         execState.classList.replace("status-halt", "status-stop");
+        execState.value = emulator.execState.error;
     }
-    if (emulator.execState.halt)
-        execState.classList.replace("status-run", "status-halt");
-    execState.value = `${emulator.execState.run ? "Running" : "Stopped"}${emulator.execState.halt ? " (Halted)" : ""}`;
     emulator.ui.elements.namedItem("ax").value = emulator.state.registers.ax.toString(16).toUpperCase().padStart(4, "0");
     emulator.ui.elements.namedItem("bx").value = emulator.state.registers.bx.toString(16).toUpperCase().padStart(4, "0");
     emulator.ui.elements.namedItem("cx").value = emulator.state.registers.cx.toString(16).toUpperCase().padStart(4, "0");
@@ -56,7 +97,46 @@ function updateDisplay() {
     for (let i = 0; i < 16; i++) {
         emulator.ui.elements.namedItem("flags" + i.toString()).checked = (emulator.state.registers.flags >> i & 1) !== 0;
     }
-    updateMemoryView();
+    {
+        const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
+        for (let i = 0; i < 16; i++) {
+            const cells = rows.item(i).querySelectorAll("td input");
+            for (let j = 0; j < 16; j++) {
+                const cell = cells.item(j);
+                cell.value = emulator.memory[emulator.base.memory + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
+            }
+        }
+    }
+    {
+        const rows = document.getElementById("program-view").querySelectorAll("tbody tr");
+        for (let i = 0; i < 16; i++) {
+            const cells = rows.item(i).querySelectorAll("td input");
+            for (let j = 0; j < 16; j++) {
+                const cell = cells.item(j);
+                cell.value = emulator.program[emulator.base.program + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
+            }
+        }
+    }
+    {
+        const rows = document.getElementById("io-reads-view").querySelectorAll("tbody tr");
+        for (let i = 0; i < 16; i++) {
+            const cells = rows.item(i).querySelectorAll("td input");
+            for (let j = 0; j < 16; j++) {
+                const cell = cells.item(j);
+                cell.value = emulator.io.reads[emulator.base.io.reads + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
+            }
+        }
+    }
+    {
+        const rows = document.getElementById("io-writes-view").querySelectorAll("tbody tr");
+        for (let i = 0; i < 16; i++) {
+            const cells = rows.item(i).querySelectorAll("td input");
+            for (let j = 0; j < 16; j++) {
+                const cell = cells.item(j);
+                cell.value = emulator.io.writes[emulator.base.io.writes + i * 16 + j].toString(16).toUpperCase().padStart(2, "0");
+            }
+        }
+    }
 }
 function stepEmulator() {
     if (emulator.execState.halt)
@@ -68,16 +148,24 @@ function stepEmulator() {
             emulator.execState.halt = true;
             break;
         case w86.W86Status.UNDEFINED_OPCODE:
-            console.error(`Undefined opcode at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`);
+            emulator.execState.run = false;
+            emulator.execState.error = `Undefined opcode at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`;
+            console.warn(emulator.execState.error);
             break;
         case w86.W86Status.UNIMPLEMENTED_OPCODE:
-            console.error(`Unimplemented opcode at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`);
+            emulator.execState.run = false;
+            emulator.execState.error = `Unimplemented opcode at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`;
+            console.error(emulator.execState.error);
             break;
         case w86.W86Status.INVALID_OPERATION:
-            console.error(`Invalid operation at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`);
+            emulator.execState.run = false;
+            emulator.execState.error = `Invalid operation at 0x${(((emulator.state.registers.cs << 4) + emulator.state.registers.ip) % (1 << 20)).toString(16).toUpperCase().padStart(5, "0")}`;
+            console.warn(emulator.execState.error);
             break;
         default:
-            console.error("Unknown error");
+            emulator.execState.run = false;
+            emulator.execState.error = "Unknown error";
+            console.error(emulator.execState.error);
     }
 }
 function runEmulator() {
@@ -111,10 +199,72 @@ function resetEmulator() {
 }
 function restartEmulator() {
     resetEmulator();
-    emulator.memory.set(emulator.rom);
+    emulator.memory.set(emulator.program);
+    emulator.io.reads.fill(0);
+    emulator.io.writes.fill(0);
 }
 {
     const rows = document.getElementById("memory-view").querySelectorAll("tbody tr");
+    const byte = document.createElement("input");
+    byte.type = "text";
+    byte.disabled = true;
+    byte.autocomplete = "off";
+    byte.required = true;
+    byte.size = 2;
+    byte.maxLength = 2;
+    byte.pattern = "[\dA-Fa-f]*";
+    byte.placeholder = "00";
+    byte.value = "00";
+    const cell = document.createElement("td");
+    cell.appendChild(byte);
+    for (const row of rows) {
+        for (let i = 0; i < 16; i++) {
+            row.appendChild(cell.cloneNode(true));
+        }
+    }
+}
+{
+    const rows = document.getElementById("program-view").querySelectorAll("tbody tr");
+    const byte = document.createElement("input");
+    byte.type = "text";
+    byte.disabled = true;
+    byte.autocomplete = "off";
+    byte.required = true;
+    byte.size = 2;
+    byte.maxLength = 2;
+    byte.pattern = "[\dA-Fa-f]*";
+    byte.placeholder = "00";
+    byte.value = "00";
+    const cell = document.createElement("td");
+    cell.appendChild(byte);
+    for (const row of rows) {
+        for (let i = 0; i < 16; i++) {
+            row.appendChild(cell.cloneNode(true));
+        }
+    }
+}
+{
+    const rows = document.getElementById("io-reads-view").querySelectorAll("tbody tr");
+    const byte = document.createElement("input");
+    byte.type = "text";
+    byte.disabled = true;
+    byte.autocomplete = "off";
+    byte.required = true;
+    byte.size = 2;
+    byte.maxLength = 2;
+    byte.pattern = "[\dA-Fa-f]*";
+    byte.placeholder = "00";
+    byte.value = "00";
+    const cell = document.createElement("td");
+    cell.appendChild(byte);
+    for (const row of rows) {
+        for (let i = 0; i < 16; i++) {
+            row.appendChild(cell.cloneNode(true));
+        }
+    }
+}
+{
+    const rows = document.getElementById("io-writes-view").querySelectorAll("tbody tr");
     const byte = document.createElement("input");
     byte.type = "text";
     byte.disabled = true;
@@ -137,13 +287,25 @@ const w86 = await W86();
 const emulator = {
     state: new w86.W86CpuState(),
     memory: new Uint8Array(),
-    rom: new Uint8Array(),
+    program: new Uint8Array(),
+    io: {
+        reads: new Uint8Array(),
+        writes: new Uint8Array(),
+    },
     execState: {
         run: false,
         halt: false
     },
     memorySize: 1048576,
-    baseAddress: 0x00000,
+    ioSize: 65536,
+    base: {
+        memory: 0x00000,
+        program: 0x00000,
+        io: {
+            reads: 0x0000,
+            writes: 0x0000
+        }
+    },
     ui: document.getElementById("emulator")
 };
 emulator.state.registers = {
@@ -163,8 +325,14 @@ emulator.state.registers = {
     flags: 0x0000
 };
 emulator.state.memory = w86._malloc(emulator.memorySize);
+emulator.state.io = {
+    reads: w86._malloc(emulator.ioSize),
+    writes: w86._malloc(emulator.ioSize)
+};
 emulator.memory = w86.HEAPU8.subarray(emulator.state.memory, emulator.state.memory + emulator.memorySize).fill(0);
-emulator.rom = new Uint8Array(new ArrayBuffer(emulator.memorySize));
+emulator.program = new Uint8Array(new ArrayBuffer(emulator.memorySize));
+emulator.io.reads = w86.HEAPU8.subarray(emulator.state.io.reads, emulator.state.io.reads + emulator.ioSize).fill(0);
+emulator.io.writes = w86.HEAPU8.subarray(emulator.state.io.writes, emulator.state.io.writes + emulator.ioSize).fill(0);
 emulator.ui.elements.namedItem("run").addEventListener("click", () => {
     emulator.execState.run = true;
     setTimeout(runEmulator, 5);
@@ -186,14 +354,26 @@ emulator.ui.elements.namedItem("restart").addEventListener("click", () => {
     restartEmulator();
     updateDisplay();
 });
+emulator.ui.elements.namedItem("example").addEventListener("change", (event) => {
+    fetch(`test/${event.currentTarget.value}.bin`).then((res) => {
+        if (!res.ok)
+            throw new Error(`Got ${res.status} ${res.statusText} when requesting ${res.url}`);
+        res.arrayBuffer().then((buf) => {
+            emulator.program.fill(0).set(new Uint8Array(buf).subarray(0, emulator.memorySize));
+            restartEmulator();
+            updateDisplay();
+        });
+    });
+});
 emulator.ui.elements.namedItem("rom").addEventListener("change", (event) => {
     event.currentTarget.files?.item(0)?.arrayBuffer().then((buf) => {
-        emulator.rom.fill(0).set(new Uint8Array(buf).subarray(0, emulator.memorySize));
+        emulator.ui.elements.namedItem("example").value = "";
+        emulator.program.fill(0).set(new Uint8Array(buf).subarray(0, emulator.memorySize));
         restartEmulator();
         updateDisplay();
     });
 });
-emulator.ui.elements.namedItem("base-address").addEventListener("change", (event) => {
+emulator.ui.elements.namedItem("memory-base").addEventListener("change", (event) => {
     const e = event.currentTarget;
     let a;
     if (!e.checkValidity()) {
@@ -202,23 +382,107 @@ emulator.ui.elements.namedItem("base-address").addEventListener("change", (event
     else {
         a = parseInt(e.value, 16);
     }
-    updateBaseAddress(a);
-    updateMemoryView();
+    updateMemoryBase(a);
+    updateDisplay();
 });
-emulator.ui.elements.namedItem("first-address").addEventListener("click", () => {
-    updateBaseAddress(0x00000);
-    updateMemoryView();
+emulator.ui.elements.namedItem("memory-first").addEventListener("click", () => {
+    updateMemoryBase(0x00000);
+    updateDisplay();
 });
-emulator.ui.elements.namedItem("prev-address").addEventListener("click", () => {
-    updateBaseAddress(emulator.baseAddress - 0x100);
-    updateMemoryView();
+emulator.ui.elements.namedItem("memory-prev").addEventListener("click", () => {
+    updateMemoryBase(emulator.base.memory - 0x100);
+    updateDisplay();
 });
-emulator.ui.elements.namedItem("next-address").addEventListener("click", () => {
-    updateBaseAddress(emulator.baseAddress + 0x100);
-    updateMemoryView();
+emulator.ui.elements.namedItem("memory-next").addEventListener("click", () => {
+    updateMemoryBase(emulator.base.memory + 0x100);
+    updateDisplay();
 });
-emulator.ui.elements.namedItem("last-address").addEventListener("click", () => {
-    updateBaseAddress(0xfff00);
-    updateMemoryView();
+emulator.ui.elements.namedItem("memory-last").addEventListener("click", () => {
+    updateMemoryBase(0xfff00);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("program-base").addEventListener("change", (event) => {
+    const e = event.currentTarget;
+    let a;
+    if (!e.checkValidity()) {
+        a = 0x00000;
+    }
+    else {
+        a = parseInt(e.value, 16);
+    }
+    updateProgramBase(a);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("program-first").addEventListener("click", () => {
+    updateProgramBase(0x00000);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("program-prev").addEventListener("click", () => {
+    updateProgramBase(emulator.base.program - 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("program-next").addEventListener("click", () => {
+    updateProgramBase(emulator.base.program + 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("program-last").addEventListener("click", () => {
+    updateProgramBase(0xfff00);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-reads-base").addEventListener("change", (event) => {
+    const e = event.currentTarget;
+    let a;
+    if (!e.checkValidity()) {
+        a = 0x0000;
+    }
+    else {
+        a = parseInt(e.value, 16);
+    }
+    updateIoReadsBase(a);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-reads-first").addEventListener("click", () => {
+    updateIoReadsBase(0x0000);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-reads-prev").addEventListener("click", () => {
+    updateIoReadsBase(emulator.base.io.reads - 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-reads-next").addEventListener("click", () => {
+    updateIoReadsBase(emulator.base.io.reads + 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-reads-last").addEventListener("click", () => {
+    updateIoReadsBase(0xff00);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-writes-base").addEventListener("change", (event) => {
+    const e = event.currentTarget;
+    let a;
+    if (!e.checkValidity()) {
+        a = 0x0000;
+    }
+    else {
+        a = parseInt(e.value, 16);
+    }
+    updateIoWritesBase(a);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-writes-first").addEventListener("click", () => {
+    updateIoWritesBase(0x0000);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-writes-prev").addEventListener("click", () => {
+    updateIoWritesBase(emulator.base.io.writes - 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-writes-next").addEventListener("click", () => {
+    updateIoWritesBase(emulator.base.io.writes + 0x100);
+    updateDisplay();
+});
+emulator.ui.elements.namedItem("io-writes-last").addEventListener("click", () => {
+    updateIoWritesBase(0xff00);
+    updateDisplay();
 });
 //# sourceMappingURL=index.js.map
